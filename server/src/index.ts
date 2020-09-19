641f4cb5f6407fdd70d68404fa1a190a12e04c39
Yevhwnii <mirandoos3223@gmail.com>
2020-09-19 17:01:49 +0300

    /refresh_token route for refreshing access tokens

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,56 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { UserResolver } from './UserResolver';
 import { createConnection } from 'typeorm';
+import cookieParser from 'cookie-parser';
+import { verify } from 'jsonwebtoken';
+import { User } from './entity/User';
+import {
+  createAccessToken,
+  createRefreshToken,
+  sendRefreshToken,
+} from './auth';
 
 (async () => {
   const app = express();
-  app.get('/', (_req, res, next) => {
+  // parsing cookie to object
+  app.use(cookieParser());
+
+  app.get('/', (_req, res) => {
     res.send('hello');
+  });
+
+  app.post('/refresh_token', async (req, res, next) => {
+    const token = req.cookies.jid;
+
+    if (!token) {
+      return res.send({ ok: false, accessToken: '' });
+    }
+    let payload: any = null;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    } catch (error) {
+      console.log(error);
+
+      return res.send({ ok: false, accessToken: '' });
+    }
+
+    // at this point token is valid and we can send back an access token
+    const user = await User.findOne({ id: payload.userId });
+    if (!user) {
+      return res.send({ ok: false, accessToken: '' });
+    }
+
+    if (user.tokenVersion !== payload.tokenVersion) {
+      return res.send({ ok: false, accessToken: '' });
+    }
+    // ensure that user can stay logged in for more than 7days if they continiously use website
+    sendRefreshToken(res, createRefreshToken(user));
+
+    return res.send({ ok: true, accessToken: createAccessToken(user) });
   });
 
   await createConnection();
@@ -17,6 +59,8 @@ import { createConnection } from 'typeorm';
     schema: await buildSchema({
       resolvers: [UserResolver],
     }),
+    // passing req and res inside apolloserver, will be passed to the resolver on every request
+    context: ({ req, res }) => ({ req, res }),
   });
 
   // Adding graphql routes as a middleware to express
@@ -26,21 +70,3 @@ import { createConnection } from 'typeorm';
     console.log('Server has started');
   });
 })();
-
-// createConnection().then(async connection => {
-
-//     console.log("Inserting a new user into the database...");
-//     const user = new User();
-//     user.firstName = "Timber";
-//     user.lastName = "Saw";
-//     user.age = 25;
-//     await connection.manager.save(user);
-//     console.log("Saved a new user with id: " + user.id);
-
-//     console.log("Loading users from the database...");
-//     const users = await connection.manager.find(User);
-//     console.log("Loaded users: ", users);
-
-//     console.log("Here you can setup and run express/koa/any other framework.");
-
-// }).catch(error => console.log(error));
