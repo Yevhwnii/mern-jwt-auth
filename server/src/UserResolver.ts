9ec9b48e8b6c1865ab5d4e3445459b4fb0eedda3
Yevhwnii <mirandoos3223@gmail.com>
2020-09-19 17:00:45 +0300

    login, revokeTokens store refresh tokens in cookie

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,14 +1,24 @@
 import { compare, hash } from 'bcryptjs';
-import { sign } from 'jsonwebtoken';
 import {
   Arg,
+  Ctx,
   Field,
+  Int,
   Mutation,
   ObjectType,
   Query,
   Resolver,
+  UseMiddleware,
 } from 'type-graphql';
+import { getConnection } from 'typeorm';
+import {
+  createAccessToken,
+  createRefreshToken,
+  sendRefreshToken,
+} from './auth';
+import { Context } from './context/context';
 import { User } from './entity/User';
+import { isAuth } from './middleware/isAuth';
 
 @ObjectType()
 class LoginResponse {
@@ -24,9 +34,27 @@ export class UserResolver {
     return 'hi!';
   }
 
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  bye(@Ctx() { payload }: Context) {
+    console.log(payload);
+
+    return `your user id is: ${payload!.userId}`;
+  }
+
   @Query(() => [User])
   users() {
     return User.find();
+  }
+
+  // if user forgot password or whatever, we can revoke tokens which will increment versions of all token by 1
+  // and afterwards they wont pass check in refresh_token route since versions will be not equal
+  @Mutation(() => Boolean)
+  async revokeRefreshTokensForUser(@Arg('userId', () => Int) userId: number) {
+    await getConnection()
+      .getRepository(User)
+      .increment({ id: userId }, 'tokenVersion', 1);
+    return true;
   }
 
   @Mutation(() => Boolean)
@@ -48,24 +76,30 @@ export class UserResolver {
     return true;
   }
 
+  //        return type of query/mutation
   @Mutation(() => LoginResponse)
   async login(
     // What user actually passes under email arg in body is assigned to our variable email
     @Arg('email') email: string,
-    @Arg('password') password: string
+    @Arg('password') password: string,
+    // Access to the context
+    @Ctx() { req, res }: Context
   ): Promise<LoginResponse> {
     const user = await User.findOne({ where: { email } });
     if (!user) {
       throw new Error('Invalid login');
     }
-    const isValid = compare(password, user.password);
+    const isValid = await compare(password, user.password);
     if (!isValid) {
       throw new Error('Bad password');
     }
-    // Login successful
+    // at this point login is successful
+
+    // Setting cookie on response with refresh token inside of it
+    sendRefreshToken(res, createRefreshToken(user));
 
     return {
-      accessToken: sign({ userId: user.id }, 'secret', { expiresIn: '15m' }),
+      accessToken: createAccessToken(user),
     };
   }
 }
